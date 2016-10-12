62b771e0630574a8c325e90e885b34ebd2fe1c6b
bryan <bryanclz@qq.com>
2016-10-12 14:41:23 +0800

    create project

---
+++ app/src/main/java/twenty_four/bryan/com/twenty_four/MainActivity.java
@@ -0,0 +1,13 @@
+package twenty_four.bryan.com.twenty_four;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
